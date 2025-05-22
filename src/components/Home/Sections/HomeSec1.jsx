9a9631d25b19ad3fcdbc08ad6422caafd4bd941c
Berth-V <menos_45@hotmail.com>
2025-05-22 16:50:13 -0700

    Frontpage icons added

--- src/components/Home/Sections/HomeSec1.jsx
+++ src/components/Home/Sections/HomeSec1.jsx
@@ -1,19 +1,23 @@
 import frontPage from '../../../assets/back-pattern.png';
-import hanspic from '../../../assets/hans-pic.jpg';
+import hanspic from '../../../assets/hans.png';
+import icon1 from '../../../assets/x-ray.png'
+import icon2 from '../../../assets/fracture.png'
+
 
 function HomeSec1() {
   return (
     <section
       className="home__section1"
-      style={{
-        background: `url(${frontPage}) no-repeat center/100%`,
-      }}
     >
       <img className="home__section1__pic" src={hanspic} />
       <div className="home__section1__textBox">
-        {/*<p className="p">Lower Back Pain, Knee, Wrist, Meniscus Rupture, Tendonitis, Among Many Other Conditions</p*/}
-        {/*<button className="home__section1__btn">Get your Appointment</button>*/}
+        <p className="home__section1__text">
+          <b className='home__section1__text --blueText'>Lower Back Pain</b>
+          , Knee, Wrist, Meniscus Rupture, Tendonitis, Among Many Other Conditions</p>
+        <button className="home__section1__btn">Get your Appointment</button>
       </div>
+      <img className='home__section1__icon1' src={icon1}/>
+      <img className='home__section1__icon2' src={icon2}/>
     </section>
   );
 }
